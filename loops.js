cb124f7d87363209c02bb8e139ba9eab98776a6b
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-03-19 00:30:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
 	for (var i = 0; i < 25; i++) {
 		if (i === 1) {
-			array.push("I am 1 strange loop.")
+			array.push("I am 1 strange loop.");
 		} else {
-			array.push(`I am ${i} strange loops.`)
+			array.push(`I am ${i} strange loops.`);
 		}
 	}
 	return array
@@ -11,7 +11,7 @@ function forLoop(array) {
 
 function whileLoop (number) {
   while ( number > 0) {
-    console.log(number)
+    console.log(number);
     number --
   }
   return "done"
